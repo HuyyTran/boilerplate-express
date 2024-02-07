16471737244955297ae9b9d70364cae23debe339
HuyyTran <huytrn.dev@gmail.com>
2024-02-07 19:00:38 +0700

    update

--- myApp.js
+++ myApp.js
@@ -1,39 +1,24 @@
-let express = require('express');
+require('dotenv').config()
+
+let express = require("express");
 let app = express();
 
+console.log("Hello World");
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+//middleware
+app.use("/public", express.static(__dirname + "/public"));
 
 
+app.get("/json", (req, res) => {
+  let response="Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    response = response.toUpperCase();
+  } 
+  res.json({ message: response });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
